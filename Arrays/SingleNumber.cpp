3136c8052a99df75c78b917b66fac05d1ad5d4a0
Pranjal150306 <jkmishra1513@gmil.com>
2025-12-19 18:44:33 +0530

    feat:solved Single Number

---
+++ Arrays/SingleNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result =0;
+        for (int i =0;i<nums.size(); i++){
+            int num = nums[i];
+            result = result^num;
+
+        }
+        return result;
+
+        
+        
+    }
+};
