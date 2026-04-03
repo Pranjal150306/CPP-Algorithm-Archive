4a687fbc50657b2724df86019a78cd3b9c15c20a
Pranjal Mishra <jkmishra1513@gmail.com>
2026-04-03 11:39:47 +0530

    Update RotatedMinimum.cpp

--- Binary-Search/RotatedMinimum.cpp
+++ Binary-Search/RotatedMinimum.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int low = 0;
-        int high = nums.size() -1;
+        int high = nums.size()-1;
         int ans = INT_MAX;
-        while (low<=high) {
+        while (low <= high) {
             int mid = low + (high-low)/2;
-            if (nums[low]<=nums[mid]) {
-                ans = min(ans , nums[low]);
-                low = mid+1;
-            }else {
-                high = mid-1;
-                ans = min(ans , nums[mid]);
-                
-            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                ans = min(ans, nums[low]);
+                low++;
+                high-- ;
+                continue ;
         }
-        return ans;
+        if (nums[low]<=nums[mid]) {
+            ans = min(ans , nums[low]);
+            low = mid + 1;
+        } else {
+            ans = min(ans , nums[mid]);
+            high = mid - 1;
+        }
+     }
+       return ans; 
     }
 };
